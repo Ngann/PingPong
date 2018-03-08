b781f6c36684c2ecf5530aa8eb4f85875b4f01e4
Guest User <Guest@Epicodus-9D.local>
2018-03-07 20:16:20 -0800

    add alert to verify

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
     event.preventDefault();
     //debugger;
     $('.questions').hide();
-    //$('.result').show(array);
-
 
 
     var q1=$("input.q1").val();
@@ -13,16 +11,18 @@ $(document).ready(function() {
     var q4=$("input.q4").val();
     var all = [q1,q2,q3,q4];
 
+    alert(all);
+    $('span#array').text(all);
 //    var array = (Q1,Q2,Q3,Q4);
 //    var Q1 = $("span#q1").text(q1);
 //    var Q2 = $("span#q2").text(q2);
 //    var Q3 = $("span#q3").text(q3);
 //    var Q4 = $("span#q4").text(q4);
 
-    $("span#q1").text(q1);
-    $("span#q2").text(q2);
-    $("span#q3").text(q3);
-    $("span#q4").text(q4);
+//    $("span#q1").text(q1);
+//    $("span#q2").text(q2);
+//    $("span#q3").text(q3);
+//    $("span#q4").text(q4);
 
     console.log('q1',q1,'q2',q2,'q3',q3,'q4',q4);
 
