71078e107871229aa630b24137c811ba63f6379d
Ngann <ngancamnguyen@gmail.com>
2018-03-12 01:49:42 -0700

    add var result to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
   $("form#ping").submit(function(event) {
     event.preventDefault();
-
     var number = parseInt($("input#number").val());
-    var result;
 
-    if (number % 5 === 0) {
+    var result = [];
+    if (number % 15 === 0){
+      result = "pingpong"
+    } else if (number % 5 === 0){
       result = 'ping'
     } else if (number % 3 === 0) {
       result = 'pong'
-    } else if (number % 15 === 0) {
-      result = 'pingpong'
     } else {
       result = 'nothing'
     }
+    $("ul#showResult").append(`<li>${number}${result}</li>`);
+  //  var add = input.push(number)
 
-    $("span#showResult").text(result);
 
-    alert(result);
 
+    // var number = parseInt($("input#number").val());
+    // var result = [];
+    //
+    // if (number % 5 === 0) {
+    //   result = 'ping'
+    // } else if (number % 3 === 0) {
+    //   result = 'pong'
+    // } else if (number % 15 === 0) {
+    //   result = 'pingpong'
+    // } else {
+    //   result = 'nothing'
+    // }
+    //
+    // $("ul#showResult").text(result);
+    console.log();
   });
 });
